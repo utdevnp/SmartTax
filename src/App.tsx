2f1494239e53df54a06eb8fa45f94c3f73743564
Utshab Luitel <utdevnp@gmail.com>
2024-12-12 22:50:14 +0545

    update calculation and update design also info

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { calculateTax } from "./helper";
+import { amountFormatter, calculateTax } from "./helper";
+let citAndSsfSumDisplay = 0;
 
 export default function App() {
   const [basicSalary, setBasicSalary] = useState(0);
@@ -13,7 +14,7 @@ export default function App() {
   const [lifeInsurance, setLifeInsurance] = useState(0);
   const [medicalInsurance, setMedicalInsurance] = useState(0);
   const [houseInsurance, setHouseInsurance] = useState(0);
-  const [maritalStatus, setMaritalStatus] = useState("MA");
+  const [maritalStatus, setMaritalStatus] = useState("S");
   const [gender, setGender] = useState("M");
 
   const [annualIncome, setAnnualIncome] = useState(0);
@@ -30,39 +31,44 @@ export default function App() {
 
     // Get annual income 
     const annualIncome = annualBasicSalary + annualDearnessAllowance + otherAllowance + festivalBonus + otherBonus;
-    setAnnualIncome(annualBasicSalary + annualDearnessAllowance + otherAllowance + festivalBonus + otherBonus);
+    setAnnualIncome(annualIncome);
 
     // Convert deductions amount to yearly
     const annualProvidentFund = providendFund * 12;
     const annualCIT = cit * 12;
 
     let citAndSsfSum = annualProvidentFund + annualCIT;
+    citAndSsfSumDisplay = annualProvidentFund + annualCIT;
 
     if (citAndSsfSum > 500000) {
       citAndSsfSum = 500000;
     } else {
       citAndSsfSum = annualProvidentFund + annualCIT;
     }
+    
+    let medicalInsuranceLimit = 0;
+    let lifeInsuranceLimit = 0;
+    let houseInsuranceLimit = 0;
 
     if (medicalInsurance > 20000) {
-      setMedicalInsurance(20000)
+      medicalInsuranceLimit = 20000;
     }
 
     if (lifeInsurance > 40000) {
-      setLifeInsurance(20000)
+      lifeInsuranceLimit = 20000;
     }
 
     if (houseInsurance > 5000) {
-      setHouseInsurance(5000)
+      houseInsuranceLimit = 5000;
     }
 
-    const annualDeductions = citAndSsfSum + houseInsurance + medicalInsurance + lifeInsurance;
+    const annualDeductions = citAndSsfSum + houseInsuranceLimit + medicalInsuranceLimit + lifeInsuranceLimit;
     setAnnualDeductions(annualDeductions);
 
     let taxableIncome: number = annualIncome - annualDeductions; // Taxable income 
     setTaxableIncome(taxableIncome);
 
-    const taxDetailArray = await calculateTax(taxableIncome);
+    const taxDetailArray = await calculateTax(taxableIncome, maritalStatus == "MA" ? false : true);
     setTaxDetail(taxDetailArray);
 
     const totalTax = taxDetailArray.reduce((sum, item: any) => {
@@ -77,7 +83,17 @@ export default function App() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-3">
-      <h1 className="text-xl font-semibold mb-4 text-center">Smart Tax Calculator</h1>
+
+      <div className="container mx-auto px-4 justify-items-center mb-4">
+        <h1 className="text-5xl font-semibold mb-4 text-center logo">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-12 text-green-500">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z" />
+          </svg>
+
+        </h1>
+        <p className="text-center"> <strong>Smart Tax Calculator</strong> <br></br>is designed to compute personal income tax based on Nepal's taxation system (Budget announcement 2080/81 ). It incorporates tax brackets, deductions, and allowances to provide accurate calculations for both married and single individuals, as well as for male and female taxpayers.</p>
+      </div>
+
 
       <div className="flex gap-6 w-full max-w-5xl">
         {/* Left Column */}
@@ -87,12 +103,12 @@ export default function App() {
               <div className="flex space-x-4 w-full">
                 <div>Are you ... </div>
                 <div>
-                  <input type="radio" id="married"  onClick={() => {{setMaritalStatus("MA") }}} name="maritalStatus" value="MA" className="mr-2" />
+                  <input type="radio" id="married" onClick={() => { { setMaritalStatus("MA") } }} checked={maritalStatus == "MA"} name="maritalStatus" value="MA" className="mr-2" />
                   <label htmlFor="married">Married</label>
                 </div>
 
                 <div>
-                  <input type="radio" id="single" name="maritalStatus" onClick={() => {{setMaritalStatus("S") }}}  value="S" className="mr-2" />
+                  <input type="radio" id="single" name="maritalStatus" onClick={() => { { setMaritalStatus("S") } }} checked={maritalStatus == "S"} value="S" className="mr-2" />
                   <label htmlFor="single">Single</label>
                 </div>
               </div>
@@ -101,12 +117,12 @@ export default function App() {
             <div className="space-y-2">
               <div className="flex space-x-4">
                 <div>
-                  <input type="radio" id="male" onClick={(e) => {setGender("M") }} name="gender" value="M" className="mr-2" />
+                  <input type="radio" id="male" onClick={(e) => { setGender("M") }} checked={gender == "M"} name="gender" value="M" className="mr-2" />
                   <label htmlFor="female">M</label>
                 </div>
 
                 <div>
-                  <input type="radio" id="female" name="gender" onClick={(e) => {setGender("F") }} value="F" className="mr-2" />
+                  <input type="radio" id="female" name="gender" onClick={(e) => { setGender("F") }} checked={gender == "F"} value="F" className="mr-2" />
                   <label htmlFor="female">F</label>
                 </div>
               </div>
@@ -146,6 +162,8 @@ export default function App() {
               </div>
             </div>
           </div>
+
+          <hr className="my-6"></hr>
 
           <h4 className=" text-xl mb-1 mt-3">Deductions</h4>
           <div className="grid grid-cols-2 gap-4 ">
@@ -187,56 +205,70 @@ export default function App() {
               <button type="submit" onClick={calculate} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Calculate</button>
             </div>
           </div>
+
+          <div className="text-center">
+            <hr className="my-4"></hr>
+            <small><strong>Disclaimer: </strong>This Tax Calculator is intended for personal use only and is not meant for commercial purposes or as a substitute for professional tax advice. While efforts have been made to ensure accuracy based on Nepal's taxation system, the results should be cross-verified, and users should consult with a certified tax professional for official calculations and filings.</small>
+          </div>
         </div>
 
         {/* Right Column */}
         <div className="w-1/3 bg-white p-4 rounded-lg shadow">
+          {
+            gender === "F" ? <div className="bg-[#83f0ab] p-3 rounded-lg justify-items-center mb-3 text-center">
+              Female Tax Rebate <b className="text-xl">{amountFormatter(annualTax * 0.10)}</b><br></br>
+              <small> <a href="" target="_blank" className="text-blue-600">How do you get rebate ?</a></small>
+            </div> : ""
+        }
 
           <div className="bg-[#f8f4f4] p-3 rounded-lg  justify-items-center mb-3">
             <h3 className="text-xl font-semibold mb-4">Annual Income </h3>
             <div className="flex items-center space-x-2">
-              <span className="text-3xl text-green-700">{annualIncome}</span>
+              <span className="text-3xl text-green-700">{amountFormatter(annualIncome)}</span>
             </div>
           </div>
 
           <div className="bg-[#f8f4f4a3] p-3 rounded-lg  justify-items-center mb-3">
             <h3 className="text-xl font-semibold mb-4">Total Deductions</h3>
             <div className="flex items-center space-x-2">
-              <span className="text-3xl text-red-700">{annualDeductions}</span>
+              <span className="text-3xl text-red-700">{amountFormatter(annualDeductions)}</span>
             </div>
+            <small className="text-blue-600">Tax relief amount <b>{amountFormatter(citAndSsfSumDisplay)}</b></small>
           </div>
 
 
           <div className="bg-[#f8f4f4a3] p-3 rounded-lg  justify-items-center mb-3">
             <h3 className="text-xl font-semibold mb-4">Taxable Income</h3>
             <div className="flex items-center space-x-2">
-              <span className="text-3xl">{taxableIncome}</span>
+              <span className="text-3xl">{amountFormatter(taxableIncome)}</span>
             </div>
           </div>
 
           <div className="bg-[#f8f4f4a3] p-3 rounded-lg  justify-items-center mb-3">
-            <h3 className="text-xl font-semibold mb-4">Total Tax</h3>
+            <h3 className="text-xl font-semibold mb-4">Annual Tax</h3>
             <div className="flex items-center space-x-2">
-              <span className="text-3xl">{annualTax}</span>
+              <span className="text-3xl">{amountFormatter(annualTax)}</span>
             </div>
+            <small className="text-blue-600">Monthly tax <b>{amountFormatter(annualTax / 12)}</b></small>
           </div>
 
           <table className="border-collapse border border-slate-400 w-full text-center ">
             <thead>
               <tr className="bg-slate-200">
-              <th className="border border-slate-300 p-2">Income</th>
-                <th className="border border-slate-300 p-2">Percentage</th>
                 <th className="border border-slate-300 p-2">Tax Slab</th>
+                <th className="border border-slate-300 p-2">Percentage</th>
+                <th className="border border-slate-300 p-2">Tax</th>
               </tr>
             </thead>
             <tbody>
               {taxDetail &&
                 taxDetail.map((item: any) => (
-                  <tr key={item.percentage} className="hover:bg-slate-100">
-                     <td className="border border-slate-300 p-2">{item.tax}</td>
+                  item.percentage ? <tr key={item.upperLimit} className="hover:bg-slate-100">
+                    <td className="border border-slate-300 p-2">{amountFormatter(item.taxableAmount)}</td>
                     <td className="border border-slate-300 p-2">{item.percentage}</td>
-                    <td className="border border-slate-300 p-2">{item.taxableAmount}</td>
+                    <td className="border border-slate-300 p-2">{amountFormatter(item.tax)}</td>
                   </tr>
+                    : ""
                 ))}
             </tbody>
           </table>
--- src/helper.ts
+++ src/helper.ts
@@ -1,47 +1,59 @@
 
-export const taxBrackets = [
-    { rate: 0.01, threshold: 500000, percentage: "1%" },
-    { rate: 0.10, threshold: 700000, percentage: "10%" },
-    { rate: 0.20, threshold: 1000000, percentage: "20%" },
-    { rate: 0.30, threshold: 2000000, percentage: "30%" },
-    { rate: 0.36, threshold: 5000000, percentage: "36%" },
-    { rate: 0.39, threshold: Infinity, percentage: "39%" }
-  ];
+export const taxBrackets = (isSingle: boolean) => {
+    return [
+        { rate: 0.01, threshold: isSingle ? 500000 : 600000, percentage: "1%" },
+        { rate: 0.10, threshold: isSingle ? 700000 : 800000, percentage: "10%" },
+        { rate: 0.20, threshold: isSingle ? 1000000 : 1100000, percentage: "20%" },
+        { rate: 0.30, threshold: 2000000, percentage: "30%" },
+        { rate: 0.36, threshold: 5000000, percentage: "36%" },
+        { rate: 0.39, threshold: Infinity, percentage: "39%" }
+    ]
+};
+
+export const amountFormatter = (number: number) => {
+    return new Intl.NumberFormat('en-IN').format(
+        number,
+    )
+}
 
 
-export const calculateTax = async (taxableAmount: number) => {
+export const calculateTax = async (taxableAmount: number, isSingle: boolean = true) => {
 
     let remainingAmount = taxableAmount;
     let totalTax = 0;
     const taxDetails = [];
+    const brackets = taxBrackets(isSingle);
 
-    for (let i = 0; i < taxBrackets.length; i++) {
-      const { rate, threshold, percentage } = taxBrackets[i];
+    for (let i = 0; i < brackets.length; i++) {
+        const { rate, threshold, percentage } = brackets[i];
 
-      if (remainingAmount > 0) {
-        const taxableInBracket = Math.min(remainingAmount, threshold - (taxDetails[i - 1]?.upperLimit || 0));
-        const taxForBracket = taxableInBracket * rate;
+        if (remainingAmount > 0) {
+            const taxableInBracket = Math.min(remainingAmount, threshold - (taxDetails[i - 1]?.upperLimit || 0));
+            const taxForBracket = taxableInBracket * rate;
 
-        taxDetails.push({
-          taxableAmount: taxableInBracket,
-          tax: taxForBracket,
-          percentage: percentage,
-          upperLimit: threshold,
-        });
+            taxDetails.push({
+                taxableAmount: taxableInBracket,
+                tax: taxForBracket,
+                percentage: percentage,
+                upperLimit: threshold,
+            });
 
-        totalTax += taxForBracket;
-        remainingAmount -= taxableInBracket;
-      } else {
-        taxDetails.push({
-          taxableAmount: 0,
-          tax: 0,
-          percentage: percentage,
-          upperLimit: threshold,
-        });
-      }
+            totalTax += taxForBracket;
+            remainingAmount -= taxableInBracket;
+        } else {
+            taxDetails.push({
+                taxableAmount: 0,
+                tax: 0,
+                percentage: percentage,
+                upperLimit: threshold,
+            });
+        }
     }
 
     taxDetails.push({ totalTax });
+    console.log("taxDetails", taxDetails);
 
     return taxDetails;
-  }
+}
+
+  
