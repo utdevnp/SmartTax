85f5afc248619cc5e791b87f9cb4a1a9ec9aac64
Utshab Luitel <utdevnp@gmail.com>
2024-12-13 19:25:04 +0545

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "SmartTax/docs",
+  base: "SmartTax",
   build: { 
     outDir: 'docs', // Set the build directory to 'docs'
   },
